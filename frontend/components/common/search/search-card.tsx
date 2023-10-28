8dc1a946857481d6d0fd55557b98cc4e4d524aca
sanjueranga <sanjueranga16@gmail.com>
2023-10-28 18:41:53 +0530

    indexed cards

--- frontend/components/common/search/search-card.tsx
+++ frontend/components/common/search/search-card.tsx
@@ -2,8 +2,18 @@ import React from "react";
 import styles from "./search.module.css";
 import { SearchResult } from "./search"; // replace with the actual path
 import { log } from "console";
+import { color } from "framer-motion";
 
-const SearchCard = ({ result }: { result: SearchResult }) => {
+const SearchCard = ({
+  result,
+  index,
+}: {
+  result: SearchResult;
+  index: number;
+}) => {
+  const colors = ["red", "green", "yellow"];
+  const right_color = colors[index % 5];
+
   return (
     <div className={styles.container} key={result.id}>
       <div className={styles.left}>
