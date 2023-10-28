826ea707b7642b1b597bbed91f43e82785e1f71d
Yasela-dissanayake <yasela2014@gmail.com>
2023-10-28 17:50:24 +0530

    Updated search result UI

--- frontend/components/common/search/search-card.tsx
+++ frontend/components/common/search/search-card.tsx
@@ -1,15 +1,28 @@
 import React from "react";
+import styles from "../search.module.css";
 import { SearchResult } from "./search"; // replace with the actual path
+import { log } from "console";
+
+
 
 const SearchCard = ({ result }: { result: SearchResult }) => {
+ 
   return (
-    <div key={result.id}>
-      <p>Title: {result.title}</p>
-      <p>Abstract ID: {result.abstractId}</p>
-      <p>Presenter: {result.presenter}</p>
+    <div className={styles.container} key={result.id}>
+      <div className={styles.left}>
+        <p className={styles.presenter}>{result.presenter}</p>
+        <p >{result.title}</p>
+      </div>
+      <div className={styles.right}>
+        <p>{result.abstractId}</p>
+        <p>{result.sessionId}</p>
+        <p className={styles.time}>{result.time}</p>
+      </div>
       {/* Display other relevant information */}
     </div>
   );
 };
 
 export default SearchCard;
+
+
