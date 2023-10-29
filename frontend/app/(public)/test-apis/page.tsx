9b58e3345b63668cc7b9cf910ce3e0314fb3f19a
kodiidok <sahan.crkodituwakku@gmail.com>
2023-10-29 12:09:14 +0530

    feat: session by category

--- frontend/app/(public)/test-apis/page.tsx
+++ frontend/app/(public)/test-apis/page.tsx
@@ -2,17 +2,20 @@ import { getSessionByCategory, getSessionItemBySessionId, searchSessionItems } f
 
 export default async function Page() {
 
-  // const data = {
-  //   hint: 'import an api handler from @/config/api and test the api integration to view response data.'
-  // };
+  const data = {
+    hint: 'import an api handler from @/config/api and test the api integration to view response data.'
+  };
 
-  const data = await getSessionByCategory('Life Sciences');
+  const sessionsByCategory = await getSessionByCategory('Life Sciences');
+  const sessionItemsBySessionId = await getSessionItemBySessionId('LS1');
 
   return (
     <div>
       we test api's here!
       <div>
         {JSON.stringify(data)}
+        {JSON.stringify(sessionsByCategory)}
+        {JSON.stringify(sessionItemsBySessionId)}
       </div>
     </div>
   );
