e8d5e59acf07a824548507845512e4eca6f836fb
kodiidok <sahan.crkodituwakku@gmail.com>
2023-10-30 02:51:06 +0530

    homepage layout and card grid

--- frontend/app/(public)/page.tsx
+++ frontend/app/(public)/page.tsx
@@ -3,13 +3,14 @@
 import { title, subtitle } from "@/components/primitives";
 import styles from '@/styles/home.module.css';
 import Banner from "@/components/common/banner/banner";
+import HomePageCards from "@/components/cards/homepage-cards";
 
 export default function Home() {
 	return (
 		<section className="flex flex-col items-center justify-center gap-4">
 			<Banner />
-			<div className="inline-block max-w-2xl text-center justify-center">
-				
+			<div className="inline-block text-center justify-center w-9/12 mt-5">
+				<HomePageCards />
 			</div>
 		</section>
 	);
--- frontend/components/common/banner/banner.tsx
+++ frontend/components/common/banner/banner.tsx
@@ -7,7 +7,27 @@ export default function Banner() {
   return (
     <>
       <div className={[styles['banner-container'], styles['center-all']].join(' ')}>
-        <Image width={450} src="rescon logo.JPG" alt="rescon 23 logo" />
+        <div className={[styles['banner-lg']].join(' ')}>
+          <div className={[styles['flex-row'], styles['flex-start']].join(' ')}>
+            <Image width={90} src="uop.png" alt="uop logo" />
+            <div className={[styles['banner-font']].join(' ')} >
+              <h1>Postgraduate Institute of Science</h1>
+              <h4>University of Peradeniya</h4>
+              <h5>Sri Lanka</h5>
+            </div>
+          </div>
+          <Image className={[styles['flex-end']].join(' ')} width={450} src="rescon logo.JPG" alt="rescon 23 logo" />
+        </div>
+        <div className={[styles['banner-sm']].join(' ')}>
+          <div className={[styles['flex-row'], styles['flex-start']].join(' ')}>
+            <Image width={90} src="uop.png" alt="uop logo" />
+            <Image className={[styles['flex-end']].join(' ')} width={450} src="rescon logo.JPG" alt="rescon 23 logo" />
+          </div>
+          <div className={[styles['banner-font']].join(' ')} >
+            <h1>Postgraduate Institute of Science</h1>
+            <h4>University of Peradeniya, Sri Lanka</h4>
+          </div>
+        </div>
       </div>
       <div className={[styles['image-container'], styles['center-all']].join(' ')}>
         <Image className={styles['image']} src="kotiya-banner.png" alt="kotiya" />
