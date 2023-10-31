7b5dbf31fbf08cb9206bfaf70da70923e371ae21
kodiidok <sahan.crkodituwakku@gmail.com>
2023-11-01 01:19:00 +0530

    fix: doker compose

--- backend/apps/server/src/main.ts
+++ backend/apps/server/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(ServerModule);
 
   app.enableCors({
-    origin: [ui],
+    origin: "*"
   });
 
   const config = new DocumentBuilder()
